f956c251db7994039a88a1f3bf39bf564b94aa46
Pham Trung <adamward459@gmail.com>
2021-01-13 14:42:43 +0700

    change value for checkbox type

--- src/admin/shared/formcontrol-input/formcontrol-input.component.ts
+++ src/admin/shared/formcontrol-input/formcontrol-input.component.ts
@@ -1,31 +1,29 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { AdminService } from '../../admin.service';
+import { Component, Input } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { AdminService } from "../../admin.service";
 
 @Component({
-  selector: 'app-formcontrol-input',
-  templateUrl: './formcontrol-input.component.html',
-  styleUrls: ['./formcontrol-input.component.scss'],
+  selector: "app-formcontrol-input",
+  templateUrl: "./formcontrol-input.component.html",
+  styleUrls: ["./formcontrol-input.component.scss"],
 })
 export class FormcontrolInputComponent {
   @Input() form: FormGroup;
   @Input() field: string;
   @Input() displayName: string;
-  @Input() inputType = 'input';
-  constructor(
-    public adminService: AdminService,
-  ) {}
+  @Input() inputType = "input";
+  constructor(public adminService: AdminService) {}
 
   /**
    * Update the form value for a boolean instance
    * @param {any} formControlValue the form control value - should be a boolean or blank string
    */
   updateFormValue(formControlValue: any) {
-    if (this.inputType === 'checkbox') {
-      formControlValue = !formControlValue;
+    if (this.inputType === "checkbox") {
+      formControlValue = !Boolean(formControlValue);
 
       this.form.patchValue({
-        [this.field]: formControlValue,
+        [this.field]: Boolean(formControlValue),
       });
     }
   }
